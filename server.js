f2efa3bff6a9f5f79fb4df83c6124c59f99f26b6
Boasty17 <36826729+Boasty17@users.noreply.github.com>
2018-03-23 21:46:21 +0000

    Update server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ client.on('guildMemberAdd', member => {
   if (!message.guild) return;
 
   // If the message content starts with "!kick"
-  if (message.content.startsWith('!kick')) {
+  if (message.content.startsWith('b!kick')) {
     // Assuming we mention someone in the message, this will return the user
     // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
     const user = message.mentions.users.first();
