d8c508eea84e4932c30fec0b03ae16998dfe1034
Boasty17 <36826729+Boasty17@users.noreply.github.com>
2018-03-23 21:35:07 +0000

    Update server.js

--- server.js
+++ server.js
@@ -108,6 +108,14 @@ client.on('message', message => {
 	  message.channel.sendEmbed(help_embed);
  }
 });
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Bienvenue a, ${member} !`);
+});
   
 
           
