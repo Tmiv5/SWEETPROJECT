9518ace7b54afa1b5b5fd9d6c06caa5ba98f6cc2
Boasty17 <36826729+Boasty17@users.noreply.github.com>
2018-08-03 15:52:41 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,55 +2,55 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 // VAR
-var BotVersion = ("1.0")
+var BotVersion = ("0.1")
 // Bot on ready
 client.on('ready', () => {
-	client.user.setPresence({ game: { name: "0.1 Beta || "+ client.guilds.size + "serveurs!" , type : 0}}); {}
+	client.user.setPresence({ game: { name: "0.1 Beta || r!help " , type : 0}}); {}
   console.log("Bot ready\nConnecté a l'espace Heroku de BlastyBot");
 });
 
 client.on('message', message => {
-  if (message.content === 'b!pingpong') {
+  if (message.content === 'r!pingpong') {
     message.channel.send('Ping-Pong! :ping_pong: ');
   }
 });
 client.on('message', message => {
-  if (message.content === 'b!newversion') {
+  if (message.content === 'r!newversion') {
     message.channel.send(' @everyone Apres plusieurs mois, on reviens en tant que Rainbow, nouveau prefix bientot, et peut etre que je recommence le bot ');
   }
 });
 
 client.on('message', message => {
-  if (message.content === 'b!help') {
+  if (message.content === 'r!help') {
 	  var help_embed = new Discord.RichEmbed()
 	  .setColor("247CFF")
-	  .setTitle("BlastyBot")
-	  .addField("Aide", "`b!help`, `b!news`, `b!info`, `b!support`, `b!invite`, `b!avatar`,`b!update`")
-	  .addField("Fun", "`b!roblox`, `b!pingpong`")
+	  .setTitle("Rainbow")
+	  .addField("Aide", "`r!help`, `r!ping`, `r!news`, `r!info`, `r!support`, `r!invite`, `r!avatar`,`r!update`")
+	  .addField("Fun", "`r!roblox`, `r!pingpong`")
 	  .setImage("https://tmiv5.weebly.com/uploads/5/5/6/5/55652459/blasty-bot-by-zenka_orig.png")
-	  .setFooter(message.author.username + " | b!help | BlastyBot " + BotVersion + " | © BlastyBot 2018-2019")
+	  .setFooter(message.author.username + " | r!help | Rainbow " + BotVersion + " | © Rainbow 2018-2019")
 	  message.channel.sendEmbed(help_embed);
     //message.channel.send('Message d`aide de BlastyBot, ```b!help : affiche ce message.\nb!ping : Affiçhe le ping.\nb!pingpong: Fais Ping-Pong!\nb!news: Donne les news du bot.\nb!roblox : Un gif roblox.\nb!info : Info a propos du bot\nb!support: Donne un serveur daide.\nb!invite : Invite le bot sur votre serveur.```\n\nBlastyBot V0.4');
   
   }
 });
 client.on('message', message => {
-  if (message.content === 'b!invite') {
-    message.reply('Voici linvitation pour inviter BlastyBot sur votre serveur\n<https://discordapp.com/oauth2/authorize?client_id=418167447032692768&scope=bot&permissions=2146958591>');
+  if (message.content === 'r!invite') {
+    message.reply("Voici l'invitation pour inviter Rainbow sur votre serveur\n<https://discordapp.com/oauth2/authorize?client_id=418167447032692768&scope=bot&permissions=2146958591>');
   }
 }); 
 client.on('message', message => {
-  if (message.content === 'b!support') {
-    message.channel.send("Voici le serveur d'aide de BlastyBot : https://discord.gg/pf9jbNB ");
+  if (message.content === 'r!support') {
+    message.channel.send("Voici le serveur d'aide de Rainbow : https://discord.gg/pf9jbNB ");
   }
 });
 client.on('message', message => {
-  if (message.content === 'b!ping') {
+  if (message.content === 'r!ping') {
     message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");
   }
 });
 client.on('message', message => {
-  if (message.content === 'b!news') {
+  if (message.content === 'r!news') {
 	  var help_embed = new Discord.RichEmbed()
 	  .setColor("247CFF")
 	  .setTitle("News")
@@ -62,13 +62,13 @@ client.on('message', message => {
 	  .addField("03-03-18", "Début du code de la Bêta 1.0\n`par Boasty`")
 	  .addField("03-03-18", "Zenka est Admin\n`par BlastyBot`")
 	  .addField("02-03-18", "Sortie de la 0.4\n`par Boasty`")
-	  .setFooter(message.author.username + " | b!news | BlastyBot " + BotVersion + " | © BlastyBot 2018-2019")
+	  .setFooter(message.author.username + " | r!news | BlastyBot " + BotVersion + " | © BlastyBot 2018-2019")
 	  message.channel.sendEmbed(help_embed);
   //  message.channel.send('**NEWS**\nSortie de la V0.4.\nDev de la 1.0 beta bientot.')
   }
 });
 client.on('message', message => {
-  if (message.content === 'b!roblox') {
+  if (message.content === 'r!roblox') {
 	  var help_embed = new Discord.RichEmbed()
 	  .setColor("247CFF")
 	  .setTitle("Rublux")
