35a8af0c19f823051d2623805bbc95bc1056d7b3
Glitch (marsbot17) <none>
2018-08-10 12:02:00 +0000

    🚌🚣 Updated with Glitch

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "m!";
-client.login("NDc3MTAzMjYxMjc5NzE1NDAz.Dk3Qog.RLgjGOVFOBpsGbtCf5qM1gS8P_M");
 client.on('ready', () => {
 client.user.setPresence({ game: { name: "IN DEV! || m!help " , type : 0}}); {}
 console.log("Bot ready");
@@ -36,21 +35,23 @@ client.on('message', message => {
   }
 });
 client.on('message', message => {
-if(message.content.startsWith(prefix + "kick")) {
-  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) 
-    return message.channel.send("Vous n'avez pas la permission!!");
-  
-  if(message.mentions.user.size === 0) {
-    return message.channe.send("Mentionne quelqun!")
-  }
-    var kick = message.guild.member(message.mentions.users.first());
-    if(!kick) {
-      return message.channel.send("Est-il en vie? :eyes: ")
+  if(message.content.startsWith(prefix + "kick")){
+        if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.channel.send("Vous n'avez pas la permission!");
+    
+        if(message.mentions.users.size === 0) {
+            return message.channel.send("Vous devez metionner un utilisaeur")
+        }
+        var kick = message.guild.member(message.mentions.users.first());
+        if(!kick) {
+            return message.channel.send("Je ne sais pas si l'utilisateur existe :/")
+        }
+    
+        if(message.guild.member(client.user).hasPermission("KICK_MEMBERS")) {
+            return message.channel.send("Je n'ai pas la permission pour kick");
+        }
+    
+        kick.kick().then(member => {
+            message.channel.send(${member.user.username} est kick par ${message.author.username});
+        });
     }
-  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) {
-    return message.channeL.send("Je n'ai pas la permission!!")
-  }
-  kick.kick().then(member => {
-    message.channel.send(`$(member.user.username) a été kick par $(message.author.username)`)
-  });
-}
+  client.login("NDc3MTAzMjYxMjc5NzE1NDAz.Dk3Qog.RLgjGOVFOBpsGbtCf5qM1gS8P_M");
