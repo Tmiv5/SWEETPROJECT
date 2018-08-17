20e6527b1d4c61952578dee17dcb4e7e5d32e9ef
Glitch (marsbot17) <none>
2018-08-17 21:55:09 +0000

    📰👗u are Updated with Glitch

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const bot = new Discord.Client();
+const util = require("util")
 var prefix = "m!";
 client.on('ready', () => {
-client.user.setPresence({ game: { name: "IN DEV! || m!help " , type : 0}}); {}
-console.log("Bot ready");
+client.user.setPresence({ game: { name: "Alpha Beta || m!help " , type : 0}}); {}
+console.log("Mars as started");
 
 });
 
@@ -20,8 +22,10 @@ if(message.content === prefix + "help"){
   .addField("Bonjour" , "Le bot vous repondera!")
   .addField("m!help", "La commande de help!")
   .addField("m!invite", "La commande d'invite du bot")
+  .addField("m!info", "Donne des info sur le bot")
+  .addField("m!say", "Le bot vous repetera!")//
   .setFooter("Help - Mars")
-    message.channel.sendEmbed(help_embed);
+  message.channel.sendEmbed(help_embed);
 }
 });
 client.on('message', message => {
@@ -35,23 +39,41 @@ client.on('message', message => {
   }
 });
 client.on('message', message => {
-  if(message.content.startsWith(prefix + "kick")){
-        if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.channel.send("Vous n'avez pas la permission!");
-    
-        if(message.mentions.users.size === 0) {
-            return message.channel.send("Vous devez metionner un utilisaeur")
-        }
-        var kick = message.guild.member(message.mentions.users.first());
-        if(!kick) {
-            return message.channel.send("Je ne sais pas si l'utilisateur existe :/")
-        }
-    
-        if(message.guild.member(client.user).hasPermission("KICK_MEMBERS")) {
-            return message.channel.send("Je n'ai pas la permission pour kick");
-        }
-    
-        kick.kick().then(member => {
-            message.channel.send(${member.user.username} est kick par ${message.author.username});
-        });
-    }
+  if(message.content === prefix + "info") {
+    message.member.send(":eyes:Info sur moi:eyes:\nNom: Mars\nCreateur: LunatiikXD\nCreer avec discord.js 11.4.0\n Merci a Alexou de cometer pour l'aide!")
+    message.reply("Regarde tes MP! :eyes: ")
+  }
+});
+client.on('message', message => {
+if(message.content.startsWith(prefix + "say")) {
+  var args = message.content.split(' ').slice(1)
+  var argresult = args.join(' ');
+  if(!argresult) return message.reply("Dis quelque chose avec m!say...")
+  message.channel.send(argresult)
+}
+});
   client.login("NDc3MTAzMjYxMjc5NzE1NDAz.Dk3Qog.RLgjGOVFOBpsGbtCf5qM1gS8P_M");
+//
+
+
+bot.on('message',message=> {
+	if(message.content.startsWith(prefix + "eval")){
+		if (message.author.id !== '405106840532156416')return message.channel.send(`**:x: You are not my owner !`)
+		var args = message.content.split(' ').slice(1)
+		let code = args.join(' ');
+		
+		try {
+		  
+		  let evaled = eval(code);
+		  let str = require("util").inspect(evaled, {
+			depth: 1
+		  })
+		  //message.channel.send("✅ Eval réussi");
+		  message.channel.send(`✅\n\n`+str.substr(0, 1800), {code:"js"});
+		} catch (err) {
+		  //message.channel.send("❌ Eval échouer");
+		  message.channel.send(`❌\n\n`+err, {code:"js"})
+		}
+	
+	}
+})
