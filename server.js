ad72c1e1746c068af639cbf2838efcaa46811dbc
Glitch (marsbot17) <none>
2018-08-20 16:13:56 +0000

    🏡🐒 Updated with Glitch

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const client = new Discord.Client();
 const bot = new Discord.Client();
 const util = require("util")
 var prefix = "m!";
+
+var token = 'NDc3MTAzMjYxMjc5NzE1NDAz.Dk3Qog.RLgjGOVFOBpsGbtCf5qM1gS8P_M';
+client.login(token);
 client.on('ready', () => {
-client.user.setPresence({ game: { name: "Alpha Beta || m!help " , type : 0}}); {}
-console.log("Mars as started");
+client.user.setPresence({ game: { name: "1.0! || m!help " , type : 0}}); {}
+console.log("Started with no error.\nVersion 1.0\n" + client.guilds.size + " serveurs.\n" + client.users.size + " Utilisateurs\n\n\nLogs :\n\n");
 
 });
 
@@ -19,13 +22,18 @@ if(message.content === prefix + "help"){
   var help_embed = new Discord.RichEmbed()
   .setColor("000000")
   .setTitle("Help!")
-  .addField("Bonjour" , "Le bot vous repondera!")
-  .addField("m!help", "La commande de help!")
+  .addField("Bonjour" , "Le bot vous répondera!")
+  .addField("m!help", "La commande d'aide du bot")
   .addField("m!invite", "La commande d'invite du bot")
   .addField("m!info", "Donne des info sur le bot")
-  .addField("m!say", "Le bot vous repetera!")//
+  .addField("m!say", "Le bot répétera ce que vous dites !")//
+  .addField("m!ping", "Donne le ping")
+  .addField("m!avatar", "Montre votre avatar")
+  .addField("m!serverinfo", "Montre les info du serv")
+  .addField("m!clear", "Efface des message!")
   .setFooter("Help - Mars")
   message.channel.sendEmbed(help_embed);
+  console.log("Un utilisateur a demandé de l'aide")
 }
 });
 client.on('message', message => {
@@ -40,10 +48,60 @@ client.on('message', message => {
 });
 client.on('message', message => {
   if(message.content === prefix + "info") {
-    message.member.send(":eyes:Info sur moi:eyes:\nNom: Mars\nCreateur: LunatiikXD\nCreer avec discord.js 11.4.0\n Merci a Alexou de cometer pour l'aide!")
+    var inf_embed = new Discord.RichEmbed()
+    .setColor('00cbff')
+    .setDescription("**Info sur moi**")
+    .addField("Nom", "Mars", true)
+    .addField("Créateur", "Lunatiik", true)
+    .addField("Version (discord.js)", `${require('discord.js').version}`)
+    .addField(`Servers`, `${client.guilds.size} serveur(s)`, false)
+    .setThumbnail(client.user.avatarURL)
+    .setFooter("Info - Mars")
+    message.member.sendEmbed(inf_embed)
+    
+  //message.member.send(`:eyes:Info sur moi:eyes:\nNom: Mars\nCreateur: LunatiikXD\nCreer avec discord.js 11.4.0\nJe suis sur ${client.guilds.size} serveurs !`)
     message.reply("Regarde tes MP! :eyes: ")
   }
 });
+client.on('message', message => {
+  if(message.content === prefix + "serverinfo") {
+    var embed = new Discord.RichEmbed()
+    .setColor('00cbff')//mec pk ta retirer C koi cette couleur Blue Ok
+    .setDescription("**Info du serveur Discord**")//tu peUx maider avec le clear? oui Mrc
+    .addField("Nom du serveur", message.guild.name, false)
+    .addField("Cree le", message.guild.createdAt, true)
+    .addField("Tu a rejoins le", message.member.joinedAt, true)
+    .addField("Membres sur le serveur", message.guild.memberCount, false)
+    .setFooter("ServerInfo - Mars")
+    message.channel.sendEmbed(embed)
+  }
+    if(message.content.startsWith(prefix + "clear")) {
+  	let args = message.content.split(' ').slice(1)
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Tu n'as pas la permission !");
+  if(!args[0]) return message.channel.send("Entres un nombre valide entre 2 et 100");
+  if (args>=101) return message.channel.send(`Quoi ? Mec c'est trop.. Voici la limite: (2-100)`)
+  message.channel.bulkDelete(args[0]).then(() => {
+  message.channel.send(`Jai effacé ${args[0]} messages.`).then(msg => msg.delete(2000));
+});
+}
+  });
+//tes sur mon bot
+    
+client.on('message', message => {
+  if(message.content === prefix + "ping")
+    message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");
+}
+          );
+client.on('message', message => {
+  if(message.content === prefix + "avatar")
+    var help_embed = new Discord.RichEmbed()
+	  .setColor("000000")
+	  .setTitle("Avatar")
+	  .setImage(message.author.avatarURL)
+	  .setFooter("Avatar - Mars")
+	  message.channel.sendEmbed(help_embed);
+  }
+);
 client.on('message', message => {
 if(message.content.startsWith(prefix + "say")) {
   var args = message.content.split(' ').slice(1)
@@ -52,13 +110,9 @@ if(message.content.startsWith(prefix + "say")) {
   message.channel.send(argresult)
 }
 });
-  client.login("NDc3MTAzMjYxMjc5NzE1NDAz.Dk3Qog.RLgjGOVFOBpsGbtCf5qM1gS8P_M");
-//
-
-
-bot.on('message',message=> {
+client.on('message', message => {
 	if(message.content.startsWith(prefix + "eval")){
-		if (message.author.id !== '405106840532156416')return message.channel.send(`**:x: You are not my owner !`)
+		if (message.author.id !== '405106840532156416') return message.channel.send("Tu n'as pas la permission")
 		var args = message.content.split(' ').slice(1)
 		let code = args.join(' ');
 		
@@ -77,3 +131,6 @@ bot.on('message',message=> {
 	
 	}
 })
+
+
+//
